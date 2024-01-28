6dad488eb197a0b4d8b42773a2eadcd5f791ae5f
sylvainYabre <sylvainyabre7@gmail.com>
2024-01-28 02:34:41 -0800

    query types defined

---
+++ src/controller/queryTypes.ts
@@ -0,0 +1,44 @@
+export type LOGIC = "AND" | "OR";
+export type MCOMPARATOR = "LT" | "GT" | "EQ";
+
+export type MFIELD= "avg" | "pass" | "fail" | "audit" | "year";
+export type SFIELD = "dept" | "id" | "instructor" | "title" | "uuid";
+
+export type IDSTRING = string;
+
+export type SKEY = `"${IDSTRING}_${SFIELD}"`;
+export type MKEY = `"${IDSTRING}_${MFIELD}"`;
+export type KEY = MKEY | SKEY;
+
+export type KEYLIST = KEY [];
+
+export interface SCOMPARISON {
+    IS: {
+        [skey in SKEY]: string;
+    };
+}
+export interface NEGATION {
+    NOT: FILTER;
+}
+
+export type LOGICCOMPARISON = {
+	[key in LOGIC]: FILTER[];
+};
+
+export type MCOMPARISON = {
+    [comparator in MCOMPARATOR]: {
+        [mkey in MKEY]: number;
+    };
+};
+
+export type FILTER = LOGICCOMPARISON | MCOMPARISON | SCOMPARISON | NEGATION;
+
+export interface OPTIONS {
+    COLUMNS: KEYLIST;
+    ORDER?: KEY;
+}
+
+export interface QUERY {
+    WHERE: FILTER | Record<never,never>; // FILTER of an empty object
+    OPTIONS: OPTIONS;
+}
