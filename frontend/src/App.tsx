71b5833adbd23a91da06a8500ca6c229dd69e5f6
Anthony Du <github@anthonydu.com>
2024-03-20 20:17:07 -0700

    Frontend setup Vite!

---
+++ frontend/src/App.tsx
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+	return (
+		<>
+			<main className="mx-auto max-w-5xl">
+				<h1>Section Insights</h1>
+				<p className="text-green-500">hello, world</p>
+			</main>
+		</>
+	);
+}
+
+export default App;
